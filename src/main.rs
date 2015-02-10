b70cbddea054ea8c725942fe917c6f9117f4985b
Luke Schoen <ltfschoen@gmail.com>
2015-02-11 09:08:07 +1100

    Added Mutable and Immutable Bindings. Used Macro with Interpolation

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,20 @@ fn main() {
     // Metaprogramming calling a macro and passing staticaly allocated string arg
     println!("Hello, world!");
 
-    // Variable binding
-    let x = 5;
+    /* 
+      Variable binding expression. 
+      <let expression> is Full Pattern for Pattern Matching.
+      Rust's Type Inference automatically determines an expression's Type.
+      Alternatively be explicit and supply an expression's Type after a colon.
+      Or simply annotate the Type in a comment to understand the Type Rust infers.
+      Rust Bindings are immutable by Default. 
+      Mutable Bindings require 'mut' prefix for Type Safety.
+    */
+    let a: i32 = 5; // assign immutable binding 'a' with Type i32 and Value 5
+    let (x, y, z) = (1, 2, 3);  // assign immutable bindings x, y, z to 1, 2, 3
+
+    let mut b: i32 = 10; // assign mutable binding 'b'
+    b = 20; // re-assign to new Value
+
+    println!("The value of b is: {}", b); // Interpret interpolation with moustaches
 }
