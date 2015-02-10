f5890aa91c5833952bd7de568797dbe5e9c0f80f
Luke Schoen <ltfschoen@gmail.com>
2015-02-11 08:31:59 +1100

    Added Variable Binding. Updated Readme. Added Comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*
+  Declare main function
+*/
 fn main() {
+
+    // Metaprogramming calling a macro and passing staticaly allocated string arg
     println!("Hello, world!");
+
+    // Variable binding
+    let x = 5;
 }
