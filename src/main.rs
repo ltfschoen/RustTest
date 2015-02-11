6fe16c7532d604149445b285cb3345aabd94ac99
Luke Schoen <ltfschoen@gmail.com>
2015-02-11 23:10:37 +1100

    Added If Expression and comments

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,32 @@ fn main() {
       Mutable Bindings require 'mut' prefix for Type Safety.
     */
     let a: i32 = 5; // assign immutable binding 'a' with Type i32 and Value 5
+
+    // If statement based on Branch concept
+    if a == 5 {
+      println!("x is five");
+    } else {
+      println!("x is not five");
+    }
+
     let (x, y, z) = (1, 2, 3);  // assign immutable bindings x, y, z to 1, 2, 3
 
     let mut b: i32 = 10; // assign mutable binding 'b'
     b = 20; // re-assign to new Value
 
-    println!("The value of b is: {}", b); // Interpret interpolation with moustaches
+    /*
+      Rust's 'If' is an Expression. It returns a value to initialise
+      the 'Let' binding ("Declaration Statement") with. 'Let' is assigned an Expression.
+      Assignment to an already bound variable is an Expression.
+      Assignment Value in Rust is the special Unit Type () in Rust's Type System.
+      In other languages 'If' is a Statements (does not return value).
+
+      Rust's "Expression Statement" converts an Expression into a Statement.
+      Semi-colons separate each Expression.
+      Semi-colons convert Expression into Statement, throws Value away, returns Unit Type ()
+      (causing Type Error when expect a different Type such as i32 to be valid)
+    */
+    let c: i32 = if b == 20 { 25 } else { 15 };  // Explicitely specify 'c' as of Integer Type
+    println!("b is: {}", b); // Interpret interpolation with moustaches
+    println!("c is: {}", c);
 }
