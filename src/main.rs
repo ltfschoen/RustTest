74da81d301033ad51b4b87c2a52c89b8939d45f0
Luke Schoen <ltfschoen@gmail.com>
2015-03-01 23:09:43 +1100

    Infinite Loop to repeat the app and Standard Input attempts

--- src/main.rs
+++ src/main.rs
@@ -43,145 +43,148 @@ fn cmp(a: u64, b: u64) -> Ordering {
 */
 fn main() {
 
-    // Standard Input
-    println!("Enter your name...");
+    loop {
 
-    // Note that the read_line() method may be called on result of stdin() and may return a line of input
-    // (i.e. when using terminal, but perhaps not for a cron job)
-    let input = old_io::stdin() // std::old_io::stdio::StdinReader
-                  .read_line() // IoResult<String> is a Generic form to match any type of input
-                  .ok() // Option<String> (assumes valid result, same as Match statement)
-                  // String (without valid value the program terminates. handles error message explicitely)
-                  .expect("Failed to read line"); 
+        // Standard Input
+        println!("Enter your name...");
 
-    // parse() function takes a &str and converts it to say u32
+        // Note that the read_line() method may be called on result of stdin() and may return a line of input
+        // (i.e. when using terminal, but perhaps not for a cron job)
+        let input = old_io::stdin() // std::old_io::stdio::StdinReader
+                      .read_line() // IoResult<String> is a Generic form to match any type of input
+                      .ok() // Option<String> (assumes valid result, same as Match statement)
+                      // String (without valid value the program terminates. handles error message explicitely)
+                      .expect("Failed to read line"); 
 
-    // The following two lines of code are equivalent
-    // where Result returned by parse is converted into an Option by using the ok method as well
-    // let input_num = input.parse::<u32>(); // input_num: Option<u32>
-    // The parse() Function receives all input from Standard Input including line breaks (i.e. \n) such as "4\n"
-    // so trim() method defined on &str to remove those aspects
-    // i32 allows both positive and negative inputs
-    let input_num: Result<i32, _> = input.trim().parse(); // input_num: Result<i32, <i32 as FromStr>::Err>
+        // parse() function takes a &str and converts it to say u32
 
-    // Unwrap input_num that has the type Option<u32> rather than u32 by using 'match'
-    let unwrapped_num = match input_num {
-        Ok(unwrapped_num) => unwrapped_num,
-        Err(_) => {
-            println!("Please input a number!");
-            return;
+        // The following two lines of code are equivalent
+        // where Result returned by parse is converted into an Option by using the ok method as well
+        // let input_num = input.parse::<u32>(); // input_num: Option<u32>
+        // The parse() Function receives all input from Standard Input including line breaks (i.e. \n) such as "4\n"
+        // so trim() method defined on &str to remove those aspects
+        // i32 allows both positive and negative inputs
+        let input_num: Result<i32, _> = input.trim().parse(); // input_num: Result<i32, <i32 as FromStr>::Err>
+
+        // Unwrap input_num that has the type Option<u32> rather than u32 by using 'match'
+        let unwrapped_num = match input_num {
+            Ok(unwrapped_num) => unwrapped_num,
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            }
+        };
+
+        // Metaprogramming calling a macro and passing staticaly allocated string arg
+        println!("Hello, {}", unwrapped_num);
+
+        /* 
+          Variable binding expression. 
+          <let expression> is Full Pattern for Pattern Matching.
+          Rust's Type Inference automatically determines an expression's Type.
+          Alternatively be explicit and supply an expression's Type after a colon.
+          Or simply annotate the Type in a comment to understand the Type Rust infers.
+          Rust Bindings are immutable by Default. 
+          Mutable Bindings require 'mut' prefix for Type Safety.
+        */
+        let a: i32 = 5; // assign immutable binding 'a' with Type i32 and Value 5
+
+        // If statement based on Branch concept
+        if a == 5 {
+          println!("x is five");
+        } else {
+          println!("x is not five");
         }
-    };
 
-    // Metaprogramming calling a macro and passing staticaly allocated string arg
-    println!("Hello, {}", unwrapped_num);
+        let (x, y, z) = (1, 2, 3);  // assign immutable bindings x, y, z to 1, 2, 3
 
-    /* 
-      Variable binding expression. 
-      <let expression> is Full Pattern for Pattern Matching.
-      Rust's Type Inference automatically determines an expression's Type.
-      Alternatively be explicit and supply an expression's Type after a colon.
-      Or simply annotate the Type in a comment to understand the Type Rust infers.
-      Rust Bindings are immutable by Default. 
-      Mutable Bindings require 'mut' prefix for Type Safety.
-    */
-    let a: i32 = 5; // assign immutable binding 'a' with Type i32 and Value 5
+        let mut b: i32 = 10; // assign mutable binding 'b'
+        b = 20; // re-assign to new Value
 
-    // If statement based on Branch concept
-    if a == 5 {
-      println!("x is five");
-    } else {
-      println!("x is not five");
+        /*
+          Rust's 'If' is an Expression. It returns a value to initialise
+          the 'Let' binding ("Declaration Statement") with. 'Let' is assigned an Expression.
+          Assignment to an already bound variable is an Expression.
+          Assignment Value in Rust is the special Unit Type () in Rust's Type System.
+          In other languages 'If' is a Statements (does not return value).
+
+          Rust's "Expression Statement" converts an Expression into a Statement.
+          Semi-colons separate each Expression.
+          Semi-colons convert Expression into Statement, throws Value away, returns Unit Type ()
+          (causing Type Error when expect a different Type such as i32 to be valid)
+        */
+        let c: i32 = if b == 20 { 25 } else { 15 };  // Explicitely specify 'c' as of Integer Type
+        println!("b is: {}", b); // Interpret interpolation with moustaches
+        println!("c is: {}", c);
+
+        incr(b, c); // Call Fn passing Args
+        print_sum(b, c); // Call Fn passing Args
+
+        // Compound Data Structure Type Tuple is simple fixed size ordered list where &str is String Slice
+        let tuple1: (i32, &str) = (1, "hello"); // Mutable. Type annotated. Without: let x = (1, "hello");
+        let mut tuple2: (i32, i32) = (1, 2); // Type annotated. Without: let x = (1, "hello");
+        let tuple3: (i32, i32) = (3, 4);
+
+        // Note that for a Tuple with a Single value the trailing commas are mandatory
+        let (x,): (i32,) = (1,);
+
+        if tuple2 != tuple3 {
+            tuple2 = tuple3; // Assign Tuple to another Mutable Tuple (with same Arity and Types)
+            println!("tuple2 and tuple3 not equal so assigning");
+        } else {
+            println!("tuple2 and tuple3 do not have same Arity, Types, Order, and Value, not assigning");
+        }
+
+        /*
+          Destructuring Let (access parameters of Tuple).
+          Let statement performs Multiple Bindings assignments when LHS and RHS Pattern Match
+        */
+        let (t1, t2) = tuple2;
+
+        println!("t1 and t2 is {}", t1 + t2); // Note: Expects t1 and t2 to be same Type
+
+        // Tuples to return Multiple Values from a Function
+        let (tnt1, tnt2) = next_two(5);
+        println!("tnt1, tnt2 = {}, {}", tnt1, tnt2);
+
+        try_structs();
+
+        try_tuple_structs();
+
+        try_newtype();
+
+        try_enums();
+
+        try_pattern_matching();
+
+        let n: i32 = 4;
+        let mut o: i32 = 0;
+
+        o = try_loops(n);
+
+        println!("o is {}", o);
+
+        // String Slices (aka String Literals) of Type &str are Statically Allocated (i.e. saved inside compiled program 
+        // and existing for entire run duration). The have fixed size and cannot be mutated (i.e. let mut). 
+        // stringLiteral is a binding reference to another string that is the Statically Allocated String Slice.
+        let mut stringLiteral: &str = "String Slice / String Literal"; // Should not be able to declare as 'let mut'
+
+        // In-Memory Strings (formerly StrBuf) allocate memorary and control their data. They cannot be converted into 
+        // the equivalent of a &'static str (Static String) like a &str, as In-Memory Strings are not pre-configured 
+        // to live for the entire lifetime of the compiled program like &str. They can infact be converted from String 
+        // into a slice &'a str by using the as_slice() method (retaining its original String lifetime)
+        let mut stringInMemory: String = "In-Memory String".to_string();
+
+        stringLiteral = try_strings(stringLiteral, stringInMemory);
+
+        println!("stringLiteral is {}", stringLiteral);
+
+        try_arrays();
+
+        try_vectors();
+
+        try_random_numbers();
     }
-
-    let (x, y, z) = (1, 2, 3);  // assign immutable bindings x, y, z to 1, 2, 3
-
-    let mut b: i32 = 10; // assign mutable binding 'b'
-    b = 20; // re-assign to new Value
-
-    /*
-      Rust's 'If' is an Expression. It returns a value to initialise
-      the 'Let' binding ("Declaration Statement") with. 'Let' is assigned an Expression.
-      Assignment to an already bound variable is an Expression.
-      Assignment Value in Rust is the special Unit Type () in Rust's Type System.
-      In other languages 'If' is a Statements (does not return value).
-
-      Rust's "Expression Statement" converts an Expression into a Statement.
-      Semi-colons separate each Expression.
-      Semi-colons convert Expression into Statement, throws Value away, returns Unit Type ()
-      (causing Type Error when expect a different Type such as i32 to be valid)
-    */
-    let c: i32 = if b == 20 { 25 } else { 15 };  // Explicitely specify 'c' as of Integer Type
-    println!("b is: {}", b); // Interpret interpolation with moustaches
-    println!("c is: {}", c);
-
-    incr(b, c); // Call Fn passing Args
-    print_sum(b, c); // Call Fn passing Args
-
-    // Compound Data Structure Type Tuple is simple fixed size ordered list where &str is String Slice
-    let tuple1: (i32, &str) = (1, "hello"); // Mutable. Type annotated. Without: let x = (1, "hello");
-    let mut tuple2: (i32, i32) = (1, 2); // Type annotated. Without: let x = (1, "hello");
-    let tuple3: (i32, i32) = (3, 4);
-
-    // Note that for a Tuple with a Single value the trailing commas are mandatory
-    let (x,): (i32,) = (1,);
-
-    if tuple2 != tuple3 {
-        tuple2 = tuple3; // Assign Tuple to another Mutable Tuple (with same Arity and Types)
-        println!("tuple2 and tuple3 not equal so assigning");
-    } else {
-        println!("tuple2 and tuple3 do not have same Arity, Types, Order, and Value, not assigning");
-    }
-
-    /*
-      Destructuring Let (access parameters of Tuple).
-      Let statement performs Multiple Bindings assignments when LHS and RHS Pattern Match
-    */
-    let (t1, t2) = tuple2;
-
-    println!("t1 and t2 is {}", t1 + t2); // Note: Expects t1 and t2 to be same Type
-
-    // Tuples to return Multiple Values from a Function
-    let (tnt1, tnt2) = next_two(5);
-    println!("tnt1, tnt2 = {}, {}", tnt1, tnt2);
-
-    try_structs();
-
-    try_tuple_structs();
-
-    try_newtype();
-
-    try_enums();
-
-    try_pattern_matching();
-
-    let n: i32 = 4;
-    let mut o: i32 = 0;
-
-    o = try_loops(n);
-
-    println!("o is {}", o);
-
-    // String Slices (aka String Literals) of Type &str are Statically Allocated (i.e. saved inside compiled program 
-    // and existing for entire run duration). The have fixed size and cannot be mutated (i.e. let mut). 
-    // stringLiteral is a binding reference to another string that is the Statically Allocated String Slice.
-    let mut stringLiteral: &str = "String Slice / String Literal"; // Should not be able to declare as 'let mut'
-
-    // In-Memory Strings (formerly StrBuf) allocate memorary and control their data. They cannot be converted into 
-    // the equivalent of a &'static str (Static String) like a &str, as In-Memory Strings are not pre-configured 
-    // to live for the entire lifetime of the compiled program like &str. They can infact be converted from String 
-    // into a slice &'a str by using the as_slice() method (retaining its original String lifetime)
-    let mut stringInMemory: String = "In-Memory String".to_string();
-
-    stringLiteral = try_strings(stringLiteral, stringInMemory);
-
-    println!("stringLiteral is {}", stringLiteral);
-
-    try_arrays();
-
-    try_vectors();
-
-    try_random_numbers();
 }
 
 /*
