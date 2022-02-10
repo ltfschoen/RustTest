a4e3000800272010558ac407db3f75a3b70d749d
Luke Schoen <ltfschoen@gmail.com>
2022-02-10 12:38:06 +0100

    refactor

--- projects/endow/src/main.rs
+++ projects/endow/src/main.rs
@@ -1,5 +1,3 @@
-// extern crate hex;
-// extern crate hex_literal;
 use scale_info::TypeInfo;
 use codec::{
   Decode, Encode,
@@ -31,24 +29,9 @@ struct Allocation {
     balances: Vec<(String, String)>
 }
 
-// #[derive(Encode, Decode, Debug, Default, Clone, Eq, PartialEq, TypeInfo)]
-// #[cfg_attr(feature = "std", derive())]
-// pub struct AllBalances {
-//     pub balances: Vec<(AccountId32, Balance)>,
-//     pub endowed: Vec<(AccountId32, Balance)>,
-// }
-
 type Signature = MultiSignature;
 type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 type Balance = u128;
-
-// fn get_allocation()
-// 	-> Result<Vec<(AccountId, Balance)>> {
-//   let balances: Vec<(AccountId, Balance)> = balances_json.into_iter().map(|e| {
-
-//   }).collect();
-//   Ok(balances)
-// }
 
 enum Error { Invalid }
 
@@ -62,11 +45,6 @@ fn get_allocation(
   let json: Allocation = serde_json::from_str(&data)?;
   let balances_json = json.balances;
   println!("balances_json {:#?}", balances_json.clone());
-
-  // let mut all_balances = AllBalances {
-  //   balances: vec![],
-  //   endowed: vec![]
-  // };
 
   let mut combined_balances: Vec<(AccountId32, Balance)> = vec![];
 
@@ -108,71 +86,6 @@ fn get_allocation(
     }
   }
 
-  // let a: Vec<(AccountId, Balance)> = balances_json.into_iter().map(|e| {
-  //   let account_public_key: String = e.0.to_string();
-  //   println!("account_public_key {:#?}", account_public_key.clone());
-  //   return account_public_key.clone();
-	// }).collect();
-
-  // let a: u8 = balances_json
-  //     .into_iter()
-  //     // .map(|x| x)
-  //     .for_each(move |e| {
-  //       let account_public_key: String = e.0.to_string();
-  //       println!("account_public_key {:#?}", account_public_key.clone());
-  //       return 1u8;
-  //     });
-  // println!("a {:#?}", a.clone());
-
-//   let BALANCE1 = 100_000u128;
-//   let BALANCE2 = 200_000u128;
-//   let initial_accounts = vec![
-//     "a6b34be9aa95c82927b112dacf99bac1e728acb0fbae849097c0f9150fa49c23",
-//     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   ];
-
-//   let json = vec![("a6b34be9aa95c82927b112dacf99bac1e728acb0fbae849097c0f9150fa49c23", )];
-//   let accountPublicKey = json[0].to_string();
-//   let accountSS58Address: AccountId32 = AccountId32::from_str(&accountPublicKey).unwrap();
-//   println!("accountPublicKey {:#?}", accountPublicKey.clone());
-//   println!("accountSS58Address {:#?}", accountSS58Address.clone());
-
-// //   let accountSS58Address = AccountId32::from_str("4MkLjys3KYVtRKBWBeNUSYxymqXK3C8vKzXZuSroWv3cVhqp").unwrap();
-
-
-//   let extra_balances = vec![
-//     (accountSS58Address.clone(), 300_000u128),
-//   ];
-// //   let a = initial_accounts
-// //       .iter()
-// //       .cloned()
-// //       .map(|x| {
-// //           // Public key (hex) of the account without the 0x prefix below
-// //           if x == "a6b34be9aa95c82927b112dacf99bac1e728acb0fbae849097c0f9150fa49c23" {
-// //               println!("initial_account treasury {:?}", x.clone());
-// //               return (x, BALANCE2);
-// //           } else {
-// //               println!("initial_account other {:?}", x.clone());
-// //               return (x, BALANCE1);
-// //           }
-// //       })
-// //       // allocate extra balances
-// //       .chain(
-// //           extra_balances.iter()
-// //               .map(|x|
-// //                   (x.0.clone(), x.1.clone())
-// //               )
-// //       )
-// //       .collect::<Vec<_>>();
-//     let a = extra_balances
-//       .iter()
-//       .cloned()
-//       .map(|x| {
-//         (x.0.clone(), x.1.clone())
-//       })
-//       .collect::<Vec<_>>();
-//   println!("finished with initial_accounts {:#?}", a.clone());
-
   return Ok(combined_balances);
 }
 
@@ -200,11 +113,12 @@ fn main() {
   let hardspoon_balances = get_allocation(endowed_accounts_with_balances.clone()).unwrap();
   println!("hardspoon_balances {:#?}", hardspoon_balances.clone());
 
-  hardspoon_balances
+  let output = hardspoon_balances
     .iter()
     .cloned()
     .map(|x| {
         return (x.0.clone(), x.1.clone());
     })
     .collect::<Vec<(AccountId, Balance)>>();
+    println!("output {:#?}", output.clone());
 }
