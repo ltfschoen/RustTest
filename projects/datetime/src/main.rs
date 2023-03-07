0b25d3a235e9b59961073154829766262991beaa
Luke Schoen <ltfschoen@gmail.com>
2023-03-07 03:38:49 +0000

    use log library

--- projects/datetime/src/main.rs
+++ projects/datetime/src/main.rs
@@ -1,4 +1,4 @@
-use log::{warn, info};
+use log::{trace, error, warn, info, debug, log_enabled};
 use thiserror::Error;
 use chrono::{
     DateTime,
@@ -18,14 +18,8 @@ struct DispatchError {
 struct ConvertI64ToU64ForDate;
 
 type Date = i64;
-type Balance = i128;
 
-struct BalanceForDate {
-    date: Date,
-    balance: Balance,
-}
-
-fn convert_i64_to_u64_in_milliseconds(date: i64) -> Result<u64, DispatchError> {
+fn convert_i64_to_u64_in_milliseconds(date: Date) -> Result<u64, DispatchError> {
     let date_as_u64_millis: u64;
     if let Some(_date_as_u64) = TryInto::<u64>::try_into(date).ok() {
         date_as_u64_millis = _date_as_u64;
@@ -36,20 +30,26 @@ fn convert_i64_to_u64_in_milliseconds(date: i64) -> Result<u64, DispatchError> {
 }
 
 fn main() {
+    ::std::env::set_var("RUST_LOG", "trace"); // or `RUST_LOG=trace cargo run`
+    log::set_max_level(log::LevelFilter::Debug);
+    env_logger::init();
+    info!("{:?}", log::max_level());
     let utc: DateTime<Utc> = Utc::now();
-    let now = utc.to_rfc3339();
-    let utc_millis: i64 = utc.timestamp_millis();
-    println!("{:?}", utc_millis);
+    let now: String = utc.to_rfc3339();
+    let utc_millis: Date = utc.timestamp_millis();
+    if log_enabled!(log::Level::Info) {
+        info!("{:?}", utc_millis);
+    }
     let _requested_date_as_u64 = convert_i64_to_u64_in_milliseconds(utc_millis.clone());
     let requested_date_as_u64;
     match _requested_date_as_u64 {
         Err(_e) => {
-            log::error!("Unable to convert i64 to u64 in millis for timestamp {:?}", _e);
+            error!("Unable to convert i64 to u64 in millis for timestamp {:?}", _e);
             return ();
         },
         Ok(ref x) => {
             requested_date_as_u64 = x;
         }
     }
-    println!("{:?}", requested_date_as_u64);
+    debug!("{:?}", requested_date_as_u64);
 }
