e209da3d16ad79ad9b0b27f5473938e0028b62e8
Luke Schoen <ltfschoen@gmail.com>
2025-01-08 14:32:53 +1100

    rearrange build tasks alphabetically

--- check_all.sh
+++ check_all.sh
@@ -1,11 +1,13 @@
 # cargo build --manifest-path ./projects/clang/Cargo.toml
 cargo build --manifest-path ./projects/closures/Cargo.toml
 cargo build --manifest-path ./projects/communicator/Cargo.toml
+# cargo build --manifest-path ./projects/endow/Cargo.toml
 cargo build --manifest-path ./projects/find_word/Cargo.toml
 cargo build --manifest-path ./projects/generics/Cargo.toml
 cargo build --manifest-path ./projects/guessing_game/Cargo.toml
 cargo build --manifest-path ./projects/hashing/Cargo.toml
 cargo build --manifest-path ./projects/iterators/Cargo.toml
+cargo build --manifest-path ./projects/jsonrpsee_pubsub/Cargo.toml
 cargo build --manifest-path ./projects/languages/Cargo.toml
 cargo build --manifest-path ./projects/lifetimes/Cargo.toml
 # cargo build --manifest-path ./projects/privacy/Cargo.toml
@@ -16,5 +18,3 @@ cargo build --manifest-path ./projects/traits_fly/Cargo.toml
 cargo build --manifest-path ./projects/traits_shapes/Cargo.toml
 # cargo build --manifest-path ./projects/unsorted/Cargo.toml
 # cargo build --manifest-path ./projects/users/Cargo.toml
-# cargo build --manifest-path ./projects/endow/Cargo.toml
-cargo build --manifest-path ./projects/jsonrpsee_pubsub/Cargo.toml
